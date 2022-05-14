a9baca3aec85bd4c744d81369891b4f33d9e5c73
Assassinsorrow <49711232+Assassinsorrow@users.noreply.github.com>
2022-05-14 22:50:57 +0200

    Added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../lib/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps, router }) {
     return (
         <ChakraProvider theme={theme}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            </Head>
             <Component {...pageProps} key={router.route} />
         </ChakraProvider>
     );
