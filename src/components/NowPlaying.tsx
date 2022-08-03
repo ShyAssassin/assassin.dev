f3a2bfb6ab4f094a00634a703fdbc6c771ca9cff
Assassinsorrow <49711232+Assassinsorrow@users.noreply.github.com>
2022-08-03 17:52:27 +0200

    Added Margin Left variable

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -16,7 +16,7 @@ function SpotifyLogo() {
     );
 }
 
-export default function NowPlaying() {
+export default function NowPlaying({ml = 0}) {
     // regex to replace everything within the brackets of a song title
     const regex = /\s*\(.*?\)\s*/g;
     // get the current songs data
@@ -26,14 +26,14 @@ export default function NowPlaying() {
     const artist = data?.artist ?? "Spotify";
     const songUrl = data?.songUrl;
     return (
-        <Box maxW={"md"} w={"full"} mb={2}>
+        <Box maxW={"md"} w={"full"} mb={2} style={{marginLeft: ml}}>
             <a href={songUrl} target="_blank" rel="noopener noreferrer">
                 <HStack spacing={1}>
                     <SpotifyLogo />
                     <Box fontWeight={"bold"} fontSize={"13"} noOfLines={1}>
                         {title}
                     </Box>
-                    <Box fontWeight={"extrabold"} fontSize={"15"}>
+                    <Box fontWeight={"extrabold"} fontSize={"17"}>
                         {" - "}
                     </Box>
                     <Box fontWeight={"normal"} fontSize={"12"} noOfLines={1}>
