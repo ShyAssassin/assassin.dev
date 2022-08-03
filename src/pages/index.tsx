8bfd5799699b8841f3fd7b32235257424e894111
Assassinsorrow <49711232+Assassinsorrow@users.noreply.github.com>
2022-08-03 17:54:02 +0200

    Started work on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Layout from "../components/layouts/layout";
 import NowPlaying from "../components/NowPlaying";
+import { Box, Heading } from "@chakra-ui/react";
+import ProfilePicture from "../components/ProfilePicture";
 
 export default function Index() {
     return (
@@ -9,7 +11,35 @@ export default function Index() {
                 <title>Assassin | Developer</title>
                 <meta name="description" content="" />
             </Head>
-            <NowPlaying />
+            {/* Intro */}
+            <Box display={"flex"}>
+                {/* Desktop specific */}
+                <Box flexGrow={1} display={{ base: "none", md: "flex" }}>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            [Assassin]
+                        </Heading>
+                        <p style={{marginLeft: 25}}>Software Engineer ( Artist / Developer / Designer )</p>
+                        <NowPlaying ml={50}/>
+                    </Box>
+                    <Box flexShrink={0} mt={{ base: 4, md: -3 }} ml={{ md: 6 }} textAlign="center">
+                        <ProfilePicture />
+                    </Box>
+                </Box>
+                {/* Mobile specific */}
+                <Box flexGrow={1} display={{ base: "inline-block", md: "none" }}>
+                    <Box flexShrink={0} mt={{ base: 4, md: -3 }} ml={"4"} textAlign="left">
+                        <ProfilePicture size="xl"/>
+                    </Box>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            [Assassin]
+                        </Heading>
+                        <NowPlaying ml={8}/>
+                        <p style={{marginLeft: 22}}>Software Engineer ( Artist / Developer / Designer )</p>
+                    </Box>
+                </Box>
+            </Box>
         </Layout>
     );
 }
