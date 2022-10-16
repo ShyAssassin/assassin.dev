602534b153742cc426a381f8a61d13132c97307e
Assassinsorrow <49711232+Assassinsorrow@users.noreply.github.com>
2022-10-16 10:29:19 +0200

    Added SVRG

--- next.config.js
+++ next.config.js
@@ -4,16 +4,24 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: false,
-    distDir: '.next',
+    distDir: ".next",
     devIndicators: {
         buildActivity: true,
-        buildActivityPosition: 'bottom-right',
+        buildActivityPosition: "bottom-right"
     },
     images: {
         domains: [
-            'i.scdn.co' // Spotify Track/Album Art
+            "i.scdn.co" // Spotify Track/Album Art
         ]
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"]
+        });
+        return config;
     }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
