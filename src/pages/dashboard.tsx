d3045713974bb35b5da40ea18fe24e37e96595a7
Assassinsorrow <49711232+Assassinsorrow@users.noreply.github.com>
2022-10-27 08:15:07 +0200

    Added dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Layout from "../components/layouts/layout";
+
+export default function Dashboard() {
+    return (
+        <Layout>
+            <Head>
+                <title>Assassin | Dashboard</title>
+                <meta name="description" content="A dashboard containing some statics" />
+            </Head>
+        </Layout>
+    );
+}
