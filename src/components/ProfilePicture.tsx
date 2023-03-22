cf0c0742d8792f1a913b3439f6c14aeeada7dacf
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-03-22 13:37:28 +0200

    Stopped using twitter CDN for profile picture

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -5,8 +5,7 @@ export default function ProfilePicture({ size = "2xl" }) {
         <Avatar
             name="Assassin"
             size={size}
-            // TODO: figure out why images load slower when using a image from `public/`
-            src={"https://pbs.twimg.com/profile_images/1602048506633588737/5fw-BC3w_400x400.jpg"}
+            src={"/images/profile-picture.png"}
             loading={"eager"}
         />
     );
