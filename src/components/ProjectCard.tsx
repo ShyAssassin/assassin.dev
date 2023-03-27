e9c033888d53b274060d140ed5d10d49064cf720
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-03-27 16:48:09 +0200

    Added some new projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,11 +6,11 @@ export default function ProjectCard({ title, image, href, description }) {
         <Box w="100%" textAlign={"center"}>
             <LinkBox cursor="pointer">
                 <LinkOverlay href={href}>
-                    <Text fontSize={"3xl"} fontWeight={"bold"}>
+                    <Text fontSize={"2xl"} fontWeight={"bold"}>
                         {title}
                     </Text>
                     <Image style={{ borderRadius: "12px" }} src={image} alt={title} placeholder="blur" />
-                    <Text mt={-1} fontSize={"15"}>
+                    <Text mt={-1} fontSize={"15"} noOfLines={2}>
                         {description}
                     </Text>
                 </LinkOverlay>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,7 +4,7 @@ import Layout from "@components/layouts/layout";
 import ProjectCard from "@components/ProjectCard";
 
 // for some reason `next/image` doesnt work if provided a URL
-import placholder from "@public/images/projects/placholder.png";
+import placholder from "@public/images/projects/placeholder.png";
 import assassin_dev from "@public/images/projects/assassin-dev.png";
 import Monarch from "@public/images/projects/monarch.png";
 
@@ -17,16 +17,28 @@ export default function Projects() {
             </Head>
             <SimpleGrid columns={[1, 2]} spacing={5}>
                 <ProjectCard
-                    title={"Assassin.dev"}
+                    title={"assassin.dev"}
                     image={assassin_dev}
-                    href={"https://github.com/Assassinsorrow/assassin.dev"}
-                    description={"My personal website and portfolio"}
+                    href={"https://github.com/ShyAssassin/assassin.dev"}
+                    description={"My personal website and portfolio (The site you are on right now)"}
+                />
+                <ProjectCard
+                    title={"ETVR Tracking Backend"}
+                    image={placholder}
+                    href={"https://github.com/ShyAssassin/ETVR-Tracking-Backend"}
+                    description={"The eye tracking backend for the EyeTrackVR project"}
+                />
+                <ProjectCard
+                    title={"PyMake"}
+                    image={placholder}
+                    href={"https://github.com/ShyAssassin/PyMake"}
+                    description={"A simple tool to add make-like functionality to Python projects"}
                 />
                 <ProjectCard
                     title={"Monarch"}
                     image={Monarch}
-                    href={"https://github.com/Assassinsorrow/Monarch"}
-                    description={"Monarch is my custom game engine inspired heavily by valve's source engine."}
+                    href={"https://github.com/ShyAssassin/Monarch"}
+                    description={"Monarch is my custom game engine inspired heavily by valve's source engine"}
                 />
             </SimpleGrid>
         </Layout>
