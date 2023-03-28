5e2cf56df03587623ad4734e27e303970681f4b5
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-03-28 19:54:02 +0200

    Update ProfilePicture.tsx

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,12 +1,5 @@
 import { Avatar } from "@chakra-ui/react";
 
 export default function ProfilePicture({ size = "2xl" }) {
-    return (
-        <Avatar
-            name="Assassin"
-            size={size}
-            src={"/images/profile-picture.png"}
-            loading={"eager"}
-        />
-    );
+    return <Avatar name="Assassin" size={size} src={"/images/profile-picture.png"} loading={"eager"} />;
 }
