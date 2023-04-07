f82fe9a580b596aa7e9677332da57b00c5b304ab
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-04-07 11:19:43 +0200

    Update now-playing.ts

--- src/pages/api/spotify/now-playing.ts
+++ src/pages/api/spotify/now-playing.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { GetNowPlaying } from "@lib/spotify";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // get data
     const response = await GetNowPlaying();
 
     // set content type; we can do this here because res.SetHeader will append new headers and not overwrite them
@@ -17,7 +16,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     // for some reason if a song was paused / stopped spotify will still return the last played song, even if `is_playing` is false.
     // also at times the song object will be empty if no song is playing, so we check for that too.
-    // if this happens, return `isPlaying` false
     if (song === null || song.item === null || !song.is_playing) {
         return res.status(200).json({ isPlaying: false }); // NOTE: this response isnt cached everytime this API is called it will make a new request to spotify
     }
@@ -30,10 +28,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const image = song.item.album.images[0].url;
     const songUrl = song.item.external_urls.spotify;
 
-    // if the song is playing; cache the response for 30 seconds
+    // cache the response for 30 seconds
     res.setHeader("Cache-Control", "public, s-maxage=30");
 
-    // return the data
     return res.status(200).json({
         isPlaying,
         title,
