b3e09dbe997c9ed1a51ad3c4775e9993d7237128
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-05-15 10:29:02 +0200

    Added `AboutMe` and `Technologies`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout from "@components/layouts/layout";
-// import Technologies from "@components/Technologies";
+import AboutMe from "@components/AboutMe";
+import Technologies from "@components/Technologies";
 import NowPlaying from "@components/NowPlaying";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import ProfilePicture from "@components/ProfilePicture";
@@ -10,9 +11,10 @@ export default function Index() {
         <Layout>
             <Head>
                 <title>Assassin | Developer</title>
-                <meta name="description" content="" />
+                <meta name="description" content="Assassin's Personal Website and portfolio" />
             </Head>
             {/* Intro */}
+            {/* TODO: Maybe make this a component? */}
             <Box display={"flex"}>
                 {/* Desktop specific */}
                 <Box flexGrow={1} display={{ base: "none", md: "flex" }}>
@@ -21,13 +23,15 @@ export default function Index() {
                             [Assassin]
                         </Heading>
                         {/* weird fucking bug, fontsize is too big on firefox on unix based systems */}
-                        <Text fontSize={19} style={{ marginLeft: 25 }} noOfLines={1}>
+                        <Text fontSize={19} marginLeft={25} noOfLines={1}>
                             Software Engineer ( Streamer / Developer / Designer )
                         </Text>
-                        {/* <p style={{ marginLeft: 50 }}>Embeded Development engineer at Valve</p> */} {/* one day :') */}
-                        <NowPlaying ml={50} />
+                        <Text marginLeft={50}>
+                            Software Engineering Intern At Amazon
+                        </Text>
+                        <NowPlaying ml={75} />
                     </Box>
-                    <Box flexShrink={0} mt={{ base: 4, md: -4 }} ml={{ md: 2 }} textAlign="center">
+                    <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 2 }} textAlign="center">
                         <ProfilePicture />
                     </Box>
                 </Box>
@@ -38,16 +42,19 @@ export default function Index() {
                             <Heading as="h2" variant="page-title">
                                 [Assassin]
                             </Heading>
-                            <Text noOfLines={1} fontSize={[16, 20]} style={{ marginLeft: 15 }}>
+                            <Text noOfLines={1} fontSize={[15, 18]} marginLeft={15}>
                                 Software Engineer ( Streamer / Developer / Designer )
                             </Text>
-                            {/* <p style={{ marginLeft: 30 }}>Embeded Development engineer at Valve</p> */} {/* one day :') */}
-                            <NowPlaying ml={30} />
+                            <Text noOfLines={1} fontSize={[15, 17]} marginLeft={30}>
+                                Software Engineering Intern At Amazon
+                            </Text>
+                            <NowPlaying ml={45} />
                         </Box>
                     </Box>
                 </Box>
             </Box>
-            {/* <Technologies /> */}
+            <Technologies />
+            <AboutMe />
         </Layout>
     );
 }
