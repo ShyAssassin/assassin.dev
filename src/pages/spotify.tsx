a3fefc46aadf0a2aa1a3f9646738d56dec2dc19b
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-07-10 20:12:16 +0200

    Delete spotify.tsx

--- src/pages/spotify.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@components/layouts/layout";
-
-export default function Spotify() {
-    return (
-        <Layout>
-            <div>Hello World, Spotify</div>
-        </Layout>
-    );
-}
