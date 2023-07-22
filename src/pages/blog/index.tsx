366b0bcc26fc4472e7d3ad0bfd2b08df91590a63
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-07-22 11:50:50 +0200

    Delete index.tsx

--- src/pages/blog/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@components/layouts/layout";
-
-export default function Blog() {
-    return (
-        <Layout>
-            <div>Hello World, blog</div>
-        </Layout>
-    );
-}
