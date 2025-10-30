ce9bec21cd4b9e5ce8c7b23a51118c53596abaa9
[Assassin] <ShyAssassin@assassin.dev>
2025-10-30 21:40:27 +0200

    I hate the web

---
+++ src/pages/api/well-known/[...slug].ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { slug } = req.query;
+
+    if (!slug || !Array.isArray(slug)) {
+        return res.status(404).send("Not Found");
+    }
+
+    const filePath = path.join(process.cwd(), "public", ".well-known", ...slug);
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            return res.status(404).send("Not Found");
+        }
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(data);
+    });
+}
