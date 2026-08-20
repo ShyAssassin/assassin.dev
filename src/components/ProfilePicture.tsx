07d9bcba7ba6a1889b8ae1ed4bebce6d3de6ed3c
[Assassin] <ShyAssassin@assassin.dev>
2026-08-20 11:50:00 +0200

    Remove prior implementation

--- src/components/ProfilePicture.tsx
+++
@@ -1,5 +0,0 @@
-import { Avatar } from "@chakra-ui/react";
-
-export default function ProfilePicture({ size = "2xl" }) {
-    return <Avatar name="Assassin" size={size} src={"/images/profile-picture.png"} loading={"eager"} />;
-}
--- src/lib/types.ts
+++
@@ -1,8 +0,0 @@
-export type Spotify_NowPlaying = {
-    isPlaying: boolean;
-    title: string;
-    artist: string;
-    album: string;
-    image: string;
-    songUrl: string;
-};
