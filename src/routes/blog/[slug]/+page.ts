e9d85be72ce73cddbe511990db62c22af0084cd1
[Assassin] <ShyAssassin@assassin.dev>
2026-08-25 22:03:50 +0200

    Fix prod blogpost loading

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,12 +1,17 @@
 import type { Post } from "$lib/types";
+import { dev } from "$app/environment";
 import type { PageLoad } from "./$types";
 
+const modules = import.meta.glob("$lib/posts/**/*.md");
 export async function load(event: Parameters<PageLoad>[0]) {
+    let postData: unknown = "Hey this shouldn't be visible, something went very extremely wrong here :p";
     const post: Post = (await event.fetch(`/api/posts?slug=${event.params.slug}`).then(res => res.json()))[0];
-    const postData = (await import(post.filePath)).default || "Huh congrats, you borked something :p";
+    if (dev) postData = (await import(/* @vite-ignore */ post.filePath)).default || "you borked something badly :)";
+    else postData = modules[post.filePath] ? (await modules[post.filePath]() as { default: unknown }).default : postData;
 
     return {
-        post: post,
-        content: postData,
+        // eslint-disable-next-line
+        content: postData as any,
+        post: post as Post,
     };
 }
